375f0364313a03b980b22a03e4d5bc6a0c657b24
Norbert Heiszler <heiszler.norbert@gmail.com>
2016-01-19 18:50:10 +0100

    travis git user addedd

--- Tests/Twig/Extension/GitExtensionTest.php
+++ Tests/Twig/Extension/GitExtensionTest.php
@@ -2,10 +2,39 @@
 
 namespace Skillberto\GitBundle\Tests\Twig\Extension;
 
-class GitExtensionTest
+use Skillberto\GitBundle\Twig\Extension\GitExtension;
+
+class GitExtensionTest extends \PHPUnit_Framework_TestCase
 {
     public function testGitVersion()
     {
+        $this->assertEquals('1.1.1', $this->getTemplate('{{git_version()}}')->render(array()));
+    }
 
+    protected function getTemplate($template)
+    {
+        $extension = new GitExtension();
+        $extension->setGitService($this->getService());
+
+        $loader = new \Twig_Loader_Array(array('index' => $template));
+        $twig = new \Twig_Environment($loader, array('debug' => true, 'cache' => false));
+        $twig->addExtension($extension);
+
+        return $twig->loadTemplate('index');
+    }
+
+    protected function getService()
+    {
+        $mock = $this
+            ->getMockBuilder('\Skillberto\GitBundle\Service\GitServiceInterface')
+            ->disableOriginalConstructor()
+            ->getMock();
+
+        $mock
+            ->expects($this->any())
+            ->method('getVersion')
+            ->will($this->returnValue('1.1.1'));
+
+        return $mock;
     }
 }
